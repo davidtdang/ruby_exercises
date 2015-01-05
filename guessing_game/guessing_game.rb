770b4d9d8270d804f827811414f8496e67a16d7f
David Dang <dtudang@gmail.com>
2015-01-05 09:33:28 -0800

    revised with string interpolation

--- guessing_game/guessing_game.rb
+++ guessing_game/guessing_game.rb
@@ -4,15 +4,15 @@ puts "Guess a number between 1 and 100"
 guess = gets.chomp.to_i
 while guess >= 1 && guess <= 100
   if guess > number
-    puts "The number is lower than " + guess.to_s + ". Guess again"
+    puts "The number is lower than #{guess.to_s}. Guess again"
     number_of_tries += 1
     guess = gets.chomp.to_i
   elsif guess < number
-    puts "The number is higher than " + guess.to_s + ". Guess again"
+    puts "The number is higher than #{guess.to_s}. Guess again"
     number_of_tries += 1
     guess = gets.chomp.to_i
   else
-    puts "You got it in " + number_of_tries.to_s + " tries"
+    puts "You got it in #{number_of_tries.to_s} tries"
     break
   end
 end
