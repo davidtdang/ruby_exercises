4f3bf8fd282551fe7365c147331c6f0247a0e838
David Dang <dtudang@gmail.com>
2015-01-05 09:44:35 -0800

    slight refactoring

--- guessing_game/guessing_game.rb
+++ guessing_game/guessing_game.rb
@@ -1,18 +1,16 @@
 number = rand(1..100)
 number_of_tries = 0
 puts "Guess a number between 1 and 100"
-guess = gets.chomp.to_i
-while guess >= 1 && guess <= 100
+guess = gets.to_i
+while guess != number
   if guess > number
     puts "The number is lower than #{guess.to_s}. Guess again"
     number_of_tries += 1
-    guess = gets.chomp.to_i
+    guess = gets.to_i
   elsif guess < number
     puts "The number is higher than #{guess.to_s}. Guess again"
     number_of_tries += 1
-    guess = gets.chomp.to_i
-  else
-    puts "You got it in #{number_of_tries.to_s} tries"
-    break
+    guess = gets.to_i
   end
 end
+puts "You got it in #{number_of_tries.to_s} tries"
